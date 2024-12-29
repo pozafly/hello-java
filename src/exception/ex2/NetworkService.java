3e40e90375d9532914ce67fad0c9da1a553ca51d
pozafly <pozafly@gmail.com>
2024-12-29 17:49:40 +0900

    feat: catch, finally

---
+++ src/exception/ex2/NetworkService.java
@@ -0,0 +1,18 @@
+package exception.ex2;
+
+public class NetworkService {
+    public void sendMessage(String data) {
+        String address = "https://example.com";
+        NetworkClient client = new NetworkClient(address);
+        client.initError(data);
+
+        try {
+            client.connect();
+            client.send(data);
+        } catch (NetworkClientException e) {
+            System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메시지: " + e.getMessage());
+        } finally {
+            client.disconnect();
+        }
+    }
+}
